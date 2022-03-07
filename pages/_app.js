85e4aebe92e9e6fe37744f83cc903f81089e0abb
Ruy Monteiro <ruystfrancis@gmail.com>
2022-03-07 01:21:51 -0300

    add themes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import '../styles/globals.css'
 
+import { useEffect } from 'react';
+import { themeChange } from 'theme-change';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const themeValues = [
+    "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"
+  ];
+
+  useEffect(() => {
+    themeChange(false);
+  }, [])
+
+  return (
+    <>
+      <select className="text-primary text-lg p-2 ml-5 mt-5 rounded" data-choose-theme>
+        <option className='text-primary' option value=''>Default Value</option>
+
+        {themeValues.map((value, index) => {
+          return (
+            <option className="text-primary" key={index} option value={value.toLocaleLowerCase()}>{value}</option>
+          )
+        })}
+      </select>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
